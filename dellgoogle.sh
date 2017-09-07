69fd0cfb735a5476acf35b2f5de0027639c73667
Yury Martynov <email@linxon.ru>
2017-09-07 15:40:32 +0300

    Initial commit

---
+++ dellgoogle.sh
@@ -0,0 +1,278 @@
+#!/bin/sh
+
+mount -o remount,rw /system
+
+rm /system/app/Google_Play.apk
+rm /system/app/Google_Play/Google_Play.apk
+rm /system/priv-app/Google_Play.apk
+rm /system/priv-app/Google_Play/Google_Play.apk
+
+rm /system/app/GoogleApps.apk
+rm /system/app/GoogleApps/GoogleApps.apk
+rm /system/priv-app/GoogleApps.apk
+rm /system/priv-app/GoogleApps/GoogleApps.apk
+
+rm /system/app/com.android.smspush.apk
+rm /system/app/com.android.smspush/com.android.smspush.apk
+rm /system/priv-app/com.android.smspush.apk
+rm /system/priv-app/com.android.smspush/com.android.smspush.apk
+
+rm /system/app/com.google.android.voicesearch.Face.Unlock.apk
+rm /system/app/com.google.android.voicesearch.Face.Unlock/com.google.android.voicesearch.Face.Unlock.apk
+rm /system/priv-app/com.google.android.voicesearch.Face.Unlock.apk
+rm /system/priv-app/com.google.android.voicesearch.Face.Unlock/com.google.android.voicesearch.Face.Unlock.apk
+
+rm /system/app/GoogleBackupTransport.apk
+rm /system/app/GoogleBackupTransport/GoogleBackupTransport.apk
+rm /system/priv-app/GoogleBackupTransport.apk
+rm /system/priv-app/GoogleBackupTransport/GoogleBackupTransport.apk
+
+rm /system/app/GoogleCalendarSyncAdapter.apk
+rm /system/app/GoogleCalendarSyncAdapter/GoogleCalendarSyncAdapter.apk
+rm /system/priv-app/GoogleCalendarSyncAdapter.apk
+rm /system/priv-app/GoogleCalendarSyncAdapter/GoogleCalendarSyncAdapter.apk
+
+rm /system/app/GoogleOneTimeInit.apk
+rm /system/app/GoogleOneTimeInit/GoogleOneTimeInit.apk
+rm /system/priv-app/GoogleOneTimeInitializer.apk
+rm /system/priv-app/GoogleOneTimeInitializer/GoogleOneTimeInitializer.apk
+
+rm /system/app/GooglePartnerSetup.apk
+rm /system/app/GooglePartnerSetup/GooglePartnerSetup.apk
+rm /system/priv-app/GooglePartnerSetup.apk
+rm /system/priv-app/GooglePartnerSetup/GooglePartnerSetup.apk
+
+rm /system/app/GoogleContactsSync.apk
+rm /system/app/GoogleContactsSync/GoogleContactsSync.apk
+rm /system/priv-app/GoogleContactsSync.apk
+rm /system/priv-app/GoogleContactsSync/GoogleContactsSync.apk
+
+rm /system/app/GoogleContactsSyncAdapter.apk
+rm /system/app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk
+rm /system/priv-app/GoogleContactsSyncAdapter.apk
+rm /system/priv-app/GoogleContactsSyncAdapter/GoogleContactsSyncAdapter.apk
+
+rm /system/app/GoogleSearch.apk
+rm /system/app/GoogleSearch/GoogleSearch.apk
+rm /system/priv-app/GoogleSearch.apk
+rm /system/priv-app/GoogleSearch/GoogleSearch.apk
+
+rm /system/app/Hangouts.apk
+rm /system/app/Hangouts/Hangouts.apk
+rm /system/priv-app/Hangouts.apk
+rm /system/priv-app/Hangouts/Hangouts.apk
+
+rm /system/app/MarketFeedbackAgent.apk
+rm /system/app/MarketFeedbackAgent/MarketFeedbackAgent.apk
+rm /system/priv-app/MarketFeedbackAgent.apk
+rm /system/priv-app/MarketFeedbackAgent/MarketFeedbackAgent.apk
+
+rm /system/app/GoogleFeedback.apk
+rm /system/app/GoogleFeedback/GoogleFeedback.apk
+rm /system/priv-app/GoogleFeedback.apk
+rm /system/priv-app/GoogleFeedback/GoogleFeedback.apk
+
+rm /system/app/News&Weather.apk
+rm /system/app/News&Weather/News&Weather.apk
+rm /system/priv-app/News&Weather.apk
+rm /system/priv-app/News&Weather/News&Weather.apk
+
+rm /system/app/OneTimeInit.apk
+rm /system/app/OneTimeInit/OneTimeInit.apk
+rm /system/priv-app/OneTimeInit.apk
+rm /system/priv-app/OneTimeInit/OneTimeInit.apk
+
+rm /system/app/PicasaUpdater.apk
+rm /system/app/PicasaUpdater/PicasaUpdater.apk
+rm /system/priv-app/PicasaUpdater.apk
+rm /system/priv-app/PicasaUpdater/PicasaUpdater.apk
+
+rm /system/app/SoundSearchforGooglePlay.apk
+rm /system/app/SoundSearchforGooglePlay/SoundSearchforGooglePlay.apk
+rm /system/priv-app/SoundSearchforGooglePlay.apk
+rm /system/priv-app/SoundSearchforGooglePlay/SoundSearchforGooglePlay.apk
+
+rm /system/app/TalkBack.apk
+rm /system/app/TalkBack/TalkBack.apk
+rm /system/priv-app/TalkBack.apk
+rm /system/priv-app/TalkBack/TalkBack.apk
+
+rm /system/app/talkBack.apk
+rm /system/app/talkBack/talkBack.apk
+rm /system/priv-app/talkBack.apk
+rm /system/priv-app/talkBack/talkBack.apk
+
+rm /system/app/talkback.apk
+rm /system/app/talkback/talkback.apk
+rm /system/priv-app/talkback.apk
+rm /system/priv-app/talkback/talkback.apk
+
+rm /system/app/Talkback.apk
+rm /system/app/Talkback/Talkback.apk
+rm /system/priv-app/Talkback.apk
+rm /system/priv-app/Talkback/Talkback.apk
+
+rm /system/app/GoogleLoginService.apk
+rm /system/app/GoogleLoginService/GoogleLoginService.apk
+rm /system/priv-app/GoogleLoginService.apk
+rm /system/priv-app/GoogleLoginService/GoogleLoginService.apk
+
+rm /system/app/GoogleServiceFramework.apk
+rm /system/app/GoogleServiceFramework/GoogleServiceFramework.apk
+rm /system/priv-app/GoogleServiceFramework.apk
+rm /system/priv-app/GoogleServiceFramework/GoogleServiceFramework.apk
+
+rm /system/app/CalendarGoogle.apk
+rm /system/app/CalendarGoogle/CalendarGoogle.apk
+rm /system/priv-app/CalendarGoogle.apk
+rm /system/priv-app/CalendarGoogle/CalendarGoogle.apk
+
+rm /system/app/FaceLock.apk
+rm /system/app/FaceLock/FaceLock.apk
+rm /system/priv-app/FaceLock.apk
+rm /system/priv-app/FaceLock/FaceLock.apk
+
+rm /system/app/GoogleAccountManager.apk
+rm /system/app/GoogleAccountManager/GoogleAccountManager.apk
+rm /system/priv-app/GoogleAccountManager.apk
+rm /system/priv-app/GoogleAccountManager/GoogleAccountManager.apk
+
+rm /system/app/GooglePlayServices.apk
+rm /system/app/GooglePlayServices/GooglePlayServices.apk
+rm /system/priv-app/GooglePlayServices.apk
+rm /system/priv-app/GooglePlayServices/GooglePlayServices.apk
+
+rm /system/app/GoogleSettingsProvider.apk
+rm /system/app/GoogleSettingsProvider/GoogleSettingsProvider.apk
+rm /system/priv-app/GoogleSettingsProvider.apk
+rm /system/priv-app/GoogleSettingsProvider/GoogleSettingsProvider.apk
+
+rm /system/app/Phonesky.apk
+rm /system/app/Phonesky/Phonesky.apk
+rm /system/priv-app/Phonesky.apk
+rm /system/priv-app/Phonesky/Phonesky.apk
+
+rm /system/app/Velvet.apk
+rm /system/app/Velvet/Velvet.apk
+rm /system/priv-app/Velvet.apk
+rm /system/priv-app/Velvet/Velvet.apk
+
+rm /system/app/GooglePinyin.apk
+rm /system/app/GooglePinyin/GooglePinyin.apk
+rm /system/priv-app/GooglePinyin.apk
+rm /system/priv-app/GooglePinyin/GooglePinyin.apk
+
+rm /system/app/Gmail.apk
+rm /system/app/Gmail/Gmail.apk
+rm /system/priv-app/Gmail.apk
+rm /system/priv-app/Gmail/Gmail.apk
+
+rm /system/app/Gmail2.apk
+rm /system/app/Gmail2/Gmail2.apk
+rm /system/priv-app/Gmail2.apk
+rm /system/priv-app/Gmail2/Gmail2.apk
+
+rm /system/app/YouTube.apk
+rm /system/app/YouTube/YouTube.apk
+rm /system/priv-app/YouTube.apk
+rm /system/priv-app/YouTube/YouTube.apk
+
+rm /system/app/Youtube.apk
+rm /system/app/Youtube/Youtube.apk
+rm /system/priv-app/Youtube.apk
+rm /system/priv-app/Youtube/Youtube.apk
+
+rm /system/app/Maps.apk
+rm /system/app/Maps/Maps.apk
+rm /system/priv-app/Maps.apk
+rm /system/priv-app/Maps/Maps.apk
+
+rm /system/app/GMS_Maps.apk
+rm /system/app/GMS_Maps/GMS_Maps.apk
+rm /system/priv-app/GMS_Maps.apk
+rm /system/priv-app/GMS_Maps/GMS_Maps.apk
+
+rm /system/app/GmsCore.apk
+rm /system/app/GmsCore/GmsCore.apk
+rm /system/priv-app/GmsCore.apk
+rm /system/priv-app/GmsCore/GmsCore.apk
+
+rm /system/app/GoogleTTS.apk
+rm /system/app/GoogleTTS/GoogleTTS.apk
+rm /system/priv-app/GoogleTTS.apk
+rm /system/priv-app/GoogleTTS/GoogleTTS.apk
+
+rm /system/recovery-from-boot.p
+
+rm /system/app/com.google.android.apps.maps.apk
+rm /system/app/com.android.vending-1.apk
+rm /system/app/gpad/GoogleVending.apk
+rm /system/app/com.google.android.setupwizard.*
+rm /system/app/VoiceSearchStub.apk
+rm /system/app/SmsReg.*
+rm /system/app/com.google.zxing.client.android.*
+rm /system/framework/com.google.android.maps.jar
+rm /system/framework/com.google.android.media.effects.jar
+rm /system/framework/com.google.widevine.software.drm.jar
+
+rm /system/app/AdupsFotaReboot.apk
+rm /system/app/AdupsFotaReboot/AdupsFotaReboot.apk
+rm /system/priv-app/AdupsFotaReboot.apk
+rm /system/priv-app/AdupsFotaReboot/AdupsFotaReboot.apk
+
+rm /system/app/AdupsFota.apk
+rm /system/app/AdupsFota/AdupsFota.apk
+rm /system/priv-app/AdupsFota.apk
+rm /system/priv-app/AdupsFota/AdupsFota.apk
+
+rm /system/app/zf_shulian_fota.apk
+rm /system/app/zf_shulian_fota/zf_shulian_fota.apk
+rm /system/priv-app/zf_shulian_fota.apk
+rm /system/priv-app/zf_shulian_fota/zf_shulian_fota.apk
+
+rm /system/app/FWUpgrade.apk
+rm /system/app/FWUpgrade/FWUpgrade.apk
+rm /system/priv-app/FWUpgrade.apk
+rm /system/priv-app/FWUpgrade/FWUpgrade.apk
+
+rm /system/app/FWUpgradeProvider.apk
+rm /system/app/FWUpgradeProvider/FWUpgradeProvider.apk
+rm /system/priv-app/FWUpgradeProvider.apk
+rm /system/priv-app/FWUpgradeProvider/FWUpgradeProvider.apk
+
+rm /system/app/GoogleEars.apk
+rm /system/app/GoogleEars/GoogleEars.apk
+rm /system/priv-app/GoogleEars.apk
+rm /system/priv-app/GoogleEars/GoogleEars.apk
+
+rm /system/app/ConfigUpdater.apk
+rm /system/app/ConfigUpdater/ConfigUpdater.apk
+rm /system/priv-app/ConfigUpdater.apk
+rm /system/priv-app/ConfigUpdater/ConfigUpdater.apk
+
+rm /system/app/GoogleServicesFramework.apk
+rm /system/app/GoogleServicesFramework/GoogleServicesFramework.apk
+rm /system/priv-app/GoogleServicesFramework.apk
+rm /system/priv-app/GoogleServicesFramework/GoogleServicesFramework.apk
+
+rm /system/app/MediaUploader.apk
+rm /system/app/MediaUploader/MediaUploader.apk
+rm /system/priv-app/MediaUploader.apk
+rm /system/priv-app/MediaUploader/MediaUploader.apk
+
+rm /system/app/WebViewGoogle.apk
+rm /system/app/WebViewGoogle/WebViewGoogle.apk
+rm /system/priv-app/WebViewGoogle.apk
+rm /system/priv-app/WebViewGoogle/WebViewGoogle.apk
+
+rm /data/app/com.mgyapp.android-1.apk
+rm /data/app/com.mgyapp.android-1/base.apk
+
+rm /data/app/com.supercleaner-1.apk
+rm /data/app/com.supercleaner-1/base.apk
+
+rm /data/app/com.mgyun.onelocker-1.apk
+rm /data/app/com.mgyun.onelocker-1/base.apk
+
+rm /data/app/com.mgyun.shua.protector-1.apk
+rm /data/app/com.mgyun.shua.protector-1/base.apk
